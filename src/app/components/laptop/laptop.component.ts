712df99efe45c3be9e6d91034ede355ce251d7b1
Sander Van den Bogaert <sander.van.den.bogaert@cubis.be>
2022-11-14 16:16:15 +0100

    sorting on all laptops added

--- src/app/components/laptop/laptop.component.ts
+++ src/app/components/laptop/laptop.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiconfigService } from 'src/app/config/apiconfig.service';
 import { Laptop } from 'src/app/_models/laptop';
-import { first } from 'rxjs';
+import { first, map } from 'rxjs';
 
 @Component({
   selector: 'app-laptop',
@@ -10,6 +10,7 @@ import { first } from 'rxjs';
 })
 export class LaptopComponent implements OnInit {
   allLaptops: Laptop[] = null;
+  sorting: string;
 
   constructor(public restApi: ApiconfigService) {}
 
@@ -25,5 +26,80 @@ export class LaptopComponent implements OnInit {
       console.log(response);
       this.ngOnInit();
     });
+  }
+
+  sortPaidLaptops(){
+    this.restApi
+    .getLaptops()
+    .pipe(map(laptops => {
+      this.allLaptops.splice(0, this.allLaptops.length)
+      for(let laptop of laptops){
+        if (laptop.isPaid == true){
+          console.log(laptop)
+          this.allLaptops.push(laptop)
+        }
+      }
+      return laptops
+    }))
+    .subscribe();
+  }
+
+  sortUnpaidLaptops(){
+    this.restApi
+    .getLaptops()
+    .pipe(map(laptops => {
+      this.allLaptops.splice(0, this.allLaptops.length)
+      for(let laptop of laptops){
+        if (laptop.isPaid == false){
+          console.log(laptop)
+          this.allLaptops.push(laptop)
+        }
+      }
+      return laptops
+    }))
+    .subscribe();
+  }
+  resetSort(){
+    this.restApi
+    .getLaptops()
+    .pipe(map(laptops => {
+      this.allLaptops.splice(0, this.allLaptops.length)
+      for(let laptop of laptops){
+        this.allLaptops.push(laptop)
+      }
+      return laptops
+    }))
+    .subscribe();
+  }
+  allLinuxLaptops(){
+    this.restApi
+    .getLaptops()
+    .pipe(map(laptops => {
+      this.allLaptops.splice(0, this.allLaptops.length)
+      for(let laptop of laptops){
+        if (laptop.type == "Linux"){
+          console.log(laptop)
+          this.allLaptops.push(laptop)
+        }
+      }
+      return laptops
+    }))
+    .subscribe();
+  }
+
+  allWindowsLaptops(){
+    this.restApi
+    .getLaptops()
+    .pipe(map(laptops => {
+      this.allLaptops.splice(0, this.allLaptops.length)
+      for(let laptop of laptops){
+        if (laptop.type == "Windows"){
+          console.log(laptop)
+          this.allLaptops.push(laptop)
+        }
+      }
+      return laptops
+    }))
+    .subscribe();
   }
 }
